c6fcb15404283ced48407ead4e337127467a097f
David Antliff <david@pitchblende.co.nz>
2025-12-26 15:31:32 +1300

    host-esp32c6: set_pixel benchmarks.

--- guest-fibonacci/src/lib.rs
+++ guest-fibonacci/src/lib.rs
@@ -5,6 +5,9 @@ extern "C" {
     fn output(num: u64);
 }
 
+extern "C" {
+    fn set_pixel(x: u32, y: u32, val: u32);
+}
 
 // Don't call the entry 'main' as it will get wrapped with C-style (argc, argv) parameters
 
@@ -42,6 +45,27 @@ pub extern "C" fn fib(mut count: u64) -> u64 {
 #[no_mangle]
 pub extern "C" fn add(x: i32, y: i32) -> i32 {
     x + y
+}
+
+#[no_mangle]
+pub extern "C" fn fill(max_x: u32, max_y: u32, val: u32) {
+    for x in 0..max_x {
+        for y in 0..max_y {
+            //let val = val.wrapping_add((x as u32).wrapping_mul(31)).wrapping_add(y as u32);
+            unsafe {
+                set_pixel(x, y, val);
+            }
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn render(max_x: u32, max_y: u32, frames: u32) {
+    let mut val = 0;
+    for f in 0..frames {
+        val += f;
+        fill(max_x, max_y, val);
+    }
 }
 
 // Panic handler required for no_std
--- host-esp32c6/src/bin/main.rs
+++ host-esp32c6/src/bin/main.rs
@@ -11,9 +11,10 @@ use core::sync::atomic::AtomicU32;
 //use defmt::info;
 use esp_hal::clock::CpuClock;
 use esp_hal::main;
-//use esp_hal::time::{Duration, Instant};
+use esp_hal::time::Instant;
 use panic_rtt_target as _;
 use esp_println::println;
+use spin::Mutex;
 
 extern crate alloc;
 
@@ -69,6 +70,17 @@ fn run_wasm() {
         }
     }).expect("Failed to define host function");
 
+    const BUFFER_WIDTH: usize = 16;
+    const BUFFER_HEIGHT: usize = 16;
+
+    let buffer = portable_atomic_util::Arc::new(Mutex::new([0u32; BUFFER_WIDTH * BUFFER_HEIGHT]));
+    let buffer_clone = buffer.clone();
+
+    linker.func_wrap("env", "set_pixel", move |x: u32, y: u32, value: u32| {
+        //println!("set_pixel called: x={}, y={}, value={}", x, y, value);
+        buffer_clone.lock()[(y as usize) * BUFFER_WIDTH + (x as usize)] = value;
+    }).expect("Failed to define host function");
+
     // 3. Instantiate the module
     println!("Instantiating instance...");
     let instance = linker.instantiate_and_start(&mut store, &module).expect("Failed to instantiate module");
@@ -84,13 +96,78 @@ fn run_wasm() {
         .get_typed_func::<(i32, i32), i32>(&mut store, "add")
         .expect("Failed to get 'add' function");
 
+    println!("Fetching 'fill' function...");
+    let fill_func = instance
+        .get_typed_func::<(u32, u32, u32), ()>(&mut store, "fill")
+        .expect("Failed to get 'fill' function");
+
+    println!("Fetching 'render' function...");
+    let render_func = instance
+        .get_typed_func::<(u32, u32, u32), ()>(&mut store, "render")
+        .expect("Failed to get 'render' function");
+
     // Call the 'add' function as a quick test
     println!("Calling 'add' function...");
     let result = add_func.call(&mut store, (41, 1)).expect("Failed to call 'add' function");
     println!("add(41, 1) returned: {result}");
 
+    // Call the 'fill' function to start the Fibonacci sequence
+    println!("Calling 'fill' function...");
+    let start = Instant::now();
+    fill_func.call(&mut store, (16, 16, 12345678)).expect("Failed to call 'fill' function");
+    let duration = start.elapsed();
+    println!("fill(16, 16, 12345678) returned: {duration}");
+    //println!("{buffer:?}");
+
+    const TARGET_FRAMES: u32 = 60;
+
+    // Measure time to fill multiple times at 8x8
+    let start = Instant::now();
+    //render.call(&mut store, (8, 8)).expect("Failed to call 'render' function");
+    for i in 0..TARGET_FRAMES {
+        fill_func.call(&mut store, (8, 8, i)).expect("Failed to call 'fill' function");
+    }
+    let duration = start.elapsed();
+    let fps = TARGET_FRAMES as f32 / (duration.as_micros() as f32 / 1_000_000.0);
+    println!("{TARGET_FRAMES} * fill(8, 8, _) took: {duration}, {fps} fps");
+
+    // Measure time to fill 60 times at 16x16
+    let start = Instant::now();
+    for i in 0..TARGET_FRAMES {
+        fill_func.call(&mut store, (16, 16, i)).expect("Failed to call 'fill' function");
+    }
+    let duration = start.elapsed();
+    let fps = TARGET_FRAMES as f32 / (duration.as_micros() as f32 / 1_000_000.0);
+    println!("{TARGET_FRAMES} * fill(16, 16, _) took: {duration}, {fps} fps");
+
+    // Measure time to fill multiple times at 8x8
+    let start = Instant::now();
+    render_func.call(&mut store, (8, 8, TARGET_FRAMES)).expect("Failed to call 'render' function");
+    let duration = start.elapsed();
+    let fps = TARGET_FRAMES as f32 / (duration.as_micros() as f32 / 1_000_000.0);
+    println!("render(8, 8, {TARGET_FRAMES}) took: {duration}, {fps} fps");
+
+    // Measure time to fill multiple times at 16x16
+    let start = Instant::now();
+    render_func.call(&mut store, (16, 16, TARGET_FRAMES)).expect("Failed to call 'render' function");
+    let duration = start.elapsed();
+    let fps = TARGET_FRAMES as f32 / (duration.as_micros() as f32 / 1_000_000.0);
+    println!("render(16, 16, {TARGET_FRAMES}) took: {duration}, {fps} fps");
+
+    // Measure time to fill 60 times at 16x16
+    let start = Instant::now();
+    for i in 0..TARGET_FRAMES {
+        fill_func.call(&mut store, (16, 16, i)).expect("Failed to call 'fill' function");
+    }
+    let duration = start.elapsed();
+    let fps = TARGET_FRAMES as f32 / (duration.as_micros() as f32 / 1_000_000.0);
+    println!("{TARGET_FRAMES} * fill(16, 16, _) took: {duration}, {fps} fps");
+
+
     // Call the 'fib' function to start the Fibonacci sequence
     println!("Calling 'fib' function...");
+    let start = Instant::now();
     let result = fib_func.call(&mut store, 100_000).expect("Failed to call 'fib' function");
-    println!("fib(100_000_000) returned: {result}");
+    let duration = start.elapsed();
+    println!("fib(100_000_000) returned: {result}, {duration}");
 }
